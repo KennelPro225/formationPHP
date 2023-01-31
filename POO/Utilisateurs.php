b4f2d1c82903f9312c1cae2649286725df832e5c
KennelPro225 <kennelkassi@gmail.com>
2023-01-31 18:29:52 +0000

    new

--- POO/Utilisateurs.php
+++ POO/Utilisateurs.php
@@ -7,19 +7,61 @@ class Utilisateurs
     private $age = 23;
     private $email;
 
-    function seConnecter()
+    public function __construct(string $nom,string $prenom,int $age,string $email)
     {
-        echo 'Je suis un utlisateur de votre application, je peux donc me connecter';
+       echo "Vous êtes ". $this->nom = $nom. " ";
+       echo $this->prenom = $prenom;
+       echo "<br>Vous avez ".$this->age = $age ." ans ";
+       echo " Et voici votre email pour que nous puissions vous contacter " . $this->email = $email;
     }
 
-    function seDeconnecter()
+    public function setNom($nom)
     {
-        echo 'Je suis un utlisateur de votre application, je peux donc me déconnecter';
+        $this->nom = $nom;
     }
 
-    public function recupererAge()
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function setEmail($email)
+    {
+        
+        if(filter_var($email,FILTER_SANITIZE_EMAIL)){
+            $this->email = $email;
+        }
+        else{
+            throw new Exception("L'information insérée n'est pas un mail, merci!");
+        }
+    }
+
+    public function setAge($age)
+    {
+        if ($age > 120 || $age < 1) {
+            throw new Exception("L'âge doit être entre 1 et 120");
+        } else {
+            $this->age = $age;
+        }
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getAge()
     {
         return $this->age;
     }
 }
-?>
--- POO/index.php
+++ POO/index.php
@@ -1,11 +1,18 @@
 <?php
 require 'Utilisateurs.php';
 
-$utilisateurs = new Utilisateurs();
+$utilisateurs = new Utilisateurs("KASSI","Kennel",20,"kennel.kassi@imako.digital");
 
-echo $utilisateurs->recupererAge();
+// echo $utilisateurs->recupererAge();
 // $utilisateurs->prenom = 'Georges Kennel Emmanuel ';
 // echo "le nom de l'utilisateur est : ". $utilisateurs->nom;
 echo "<br>";
-echo $utilisateurs->seConnecter();
+// echo $utilisateurs->seConnecter();
+
+// $utilisateurs->setNom("KASSI");
+// $utilisateurs->setAge("22");
+// $utilisateurs->setPrenom("Kennel");
+// $utilisateurs->setEmail("kennel.kassi@imako.digital");
+// echo "<br>";
+// echo $utilisateurs->getAge();
 ?>
